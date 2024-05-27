27a3273e88948a2005f413a1df3320c0d1b4d07d
Af1991 <152735889+Afer1991@users.noreply.github.com>
2024-05-27 17:17:37 -0400

    Create regex array and function to check for valid numbers

--- script.js
+++ script.js
@@ -3,9 +3,24 @@ const results = document.getElementById("results-div");
 const checkBtn = document.getElementById("check-btn");
 const clearBtn = document.getElementById("clear-btn");
 
+const regex1 = /[0-9]{10}/;
+
+const regexArray = [regex1];
+
+const isValidNumber = (input) => {
+  const isValid = regexArray.some(regex => regex.test(input));
+  if (isValid) {
+    results.innerText += `Valid US number: ${input}`;
+  } else {
+    results.innerText += `Invalid US number: ${input}`;
+  }
+};
+
 const checkNumber = () => {
   if (input.value === "") {
     alert("Please provide a phone number");
+  } else {
+    isValidNumber(input.value);
   }
 };
 
@@ -14,4 +29,8 @@ checkBtn.addEventListener("keydown", (e) => {
   if(e.key === "Enter") {
     checkNumber();
   };
+});
+
+clearBtn.addEventListener("click", () => {
+  results.innerText = "";
 });
